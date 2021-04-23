42c894d870e8627bbc2f70efcb3cda4bb023d365
Beadhangerone <denm.2206@gmail.com>
2021-04-23 10:20:08 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * SEP4IOT.c
+ *
+ * Created: 23/04/2021 10:01:23 AM
+ * Author : VIACoffeBeans
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
