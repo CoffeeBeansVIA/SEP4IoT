4c2c11b35c78ba333a226e1aa75adf32041bde6d
Beadhangerone <denm.2206@gmail.com>
2021-05-26 22:48:53 +0200

    welcome message

--- upLinkHandler.h
+++ upLinkHandler.h
@@ -12,6 +12,7 @@
 //----------------------------FUNCTIONS----------------------------
 void UL_handler_create();
 void UL_receive_task( void *pvParameters );
+void Welcome_task(void *pvParams);
 void _lora_setup();
 
 //-----------------------------------------------------------------
